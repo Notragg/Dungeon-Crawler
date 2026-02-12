d6f256ce842147109b1df6107823acf4cd424d09
Shravan Goyal <shravangoyal345@gmail.com>
2026-02-12 10:30:07 +0530

    Create shintest.cpp

---
+++ Testing/shintest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <initializer_list>
+#include "X:\DUNGEONCRAWLER\Dungeon-Crawler\classes\mobs.h"
+
+
+int main(){
+    srand(time(0));
+    
+    Enemy Medrada("Medrada" , 25 , 50 , 40 , 1000 , 0 ) ; // name(Ename) , hp(health) , atk(attack) , def(defense) , wraith(wrath) , loot(mon)
+    Medrada.addDialogues({
+        "WHYYYYYYYYYY!!!???",
+        "**She looks at you with a tearing eye**",
+        "YOU KNOW I WAS AN ADVENTURER TOO!!!",
+        "**she smiles a little**",
+        "If possible , i dont wanna fight.",
+        "THIS CURSE DRIVES ME CRAZY",
+        "I swear this is not me , this is the curse.",
+        "So it is my mistake????? is it????"
+    });
+
+    
+};
