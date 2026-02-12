636a18b3be131bd24bf11f5ad49e01dddb7677b2
Shravan Goyal <shravangoyal345@gmail.com>
2026-02-12 10:14:10 +0530

    Update main.cpp

--- classes/main.cpp
+++ classes/main.cpp
@@ -8,16 +8,17 @@
 
 int main(){
 srand(time(0));
-Enemy Scorpion("Scorpion" , 90 , 45 , 15 , 2 , 100);
+Enemy Scorpion("Scorpion" , 90 , 45 , 15 , 2 , 100); // name(Ename) , hp(health) , atk(attack) , def(defense) , wraith(wrath) , loot(mon)
 Scorpion.addDialogues({
-    "*Hissss*",
-    "*Click-click*",
-    "*Rattle of chitin*",
-    "*Sharp tail snaps in the air*",
-    "*Low aggressive screech*",
-    "*Dry sand shifts as it moves*",
-    "*Venom drips from its stinger*"
+    "*Hissss*\n",
+    "*Click-click*\n",
+    "*Rattle of chitin*\n",
+    "*Sharp tail snaps in the air*\n",
+    "*Low aggressive screech*\n",
+    "*Dry sand shifts as it moves*\n",
+    "*Venom drips from its stinger*\n"
 });
+
 
 
 
